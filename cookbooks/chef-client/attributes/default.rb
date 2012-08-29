bb78e3843b9707bde1984550d2f65a350af1dfc7
Rob LaRubbio <rob@larubbio.org>
2012-08-28 22:10:25 -0700

    Chef updates

--- cookbooks/chef-client/attributes/default.rb
+++ cookbooks/chef-client/attributes/default.rb
@@ -25,7 +25,7 @@ default["chef_client"]["splay"]       = "20"
 default["chef_client"]["log_dir"]     = "/var/log/chef"
 default["chef_client"]["conf_dir"]    = "/etc/chef"
 default["chef_client"]["bin"]         = "/usr/bin/chef-client"
-default["chef_client"]["server_url"]  = "http://localhost:4000"
+default["chef_client"]["server_url"]  = "https://api.opscode.com/organizations/motech"
 default["chef_client"]["validation_client_name"] = "chef-validator"
 default["chef_client"]["cron"] = { "minute" => "0", "hour" => "*/4" }
 
