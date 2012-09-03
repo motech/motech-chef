ffc0fc927936b2513bbeb6b09ec7533777411774
Paweł Gesek <pgesek@soldevelo.com>
2012-09-03 18:04:36 +0200

    Updated cookbooks, loading openmrs database

--- cookbooks/motech-demo/attributes/default.rb
+++ cookbooks/motech-demo/attributes/default.rb
@@ -1,1 +1,5 @@
 set['motech_demo']['build_dir'] = "/usr/share/motech-demo-build"
+
+set['motech_demo']['openmrs']['db']['name'] = "motech_openmrs_api"
+set['motech_demo']['openmrs']['db']['user'] = "openmrs"
+set['motech_demo']['openmrs']['db']['password'] = "password"
--- cookbooks/motech-demo/recipes/default.rb
+++ cookbooks/motech-demo/recipes/default.rb
@@ -26,8 +26,41 @@ template "#{node['motech_demo']['build_dir']}/pom.xml" do
 	mode "0644"
 end
 
+template "#{node['motech_demo']['build_dir']}/openmrs-1.8.sql" do
+	source "openmrs-1.8.sql.erb"
+	owner "root"
+	mode "0644"
+end
+
+script "bootstrap" do
+	user "root"
+	interpreter "bash"
+	code <<-EOH
+	MYSQL_PASS=#{node['mysql']['server_root_password']}
+	BUILD_DIR=#{node['motech_demo']['build_dir']}
+	OPENMRS_DB=#{node['motech_demo']['openmrs']['db']['name']}
+	OPENMRS_USER=#{node['motech_demo']['openmrs']['db']['user']}
+	OPENMRS_PASS=#{node['motech_demo']['openmrs']['db']['password']}
+	
+	mysql -u root -p$MYSQL_PASS -e "DROP DATABASE IF EXISTS $OPENMRS_DB; CREATE DATABASE $OPENMRS_DB DEFAULT CHARSET utf8;"
+        mysql -u root -p$MYSQL_PASS -e "GRANT ALL ON $OPENMRS_DB.* TO '$OPENMRS_USER'@'localhost' IDENTIFIED BY '$OPENMRS_PASS';"
+        mysql -u root -p$MYSQL_PASS -e "GRANT ALL ON $OPENMRS_DB.* TO '$OPENMRS_USER'@'%' IDENTIFIED BY '$OPENMRS_PASS';"
+        mysql -u root -p$MYSQL_PASS -e "FLUSH PRIVILEGES;"	
+
+	mysql -u $OPENMRS_USER -p$OPENMRS_PASS $OPENMRS_DB < $BUILD_DIR/openmrs-1.8.sql
+	EOH
+end
+
+script "build" do
+	user "root"
+	interpreter "bash"
+	code <<-EOH
+		#{node['motech_demo']['build_dir']}/build.sh
+	EOH
+end
+
 cron "rebuild" do
-  hour "5"
-  minute "0"
-  command "#{node['motech_demo']['build_dir']}/build.sh"
+	hour "5"
+	minute "0"
+	command "#{node['motech_demo']['build_dir']}/build.sh"
 end
