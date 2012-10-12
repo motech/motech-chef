9f8d3de3444563f384bc704d09e8020a51805335
Paweł Gesek <pgesek@soldevelo.com>
2012-10-12 11:05:12 +0200

    SchedulerSupport=true in activemq config

--- cookbooks/motech-jenkins/recipes/default.rb
+++ cookbooks/motech-jenkins/recipes/default.rb
@@ -7,6 +7,10 @@
 # All rights reserved - Do Not Redistribute
 #
 
+version = node['activemq']['version']
+mirror = node['activemq']['mirror']
+activemq_home = "#{node['activemq']['home']}/apache-activemq-#{node['activemq']['version']}"
+
 template "#{node[:jenkins][:server][:home]}/hudson.plugins.git.GitSCM.xml" do
 	source "hudson.plugins.git.GitSCM.xml.erb"
 	owner "#{node[:jenkins][:server][:user]}"
@@ -17,5 +21,10 @@ template "#{node[:jenkins][:server][:home]}/hudson.tasks.Maven.xml" do
         source "hudson.tasks.Maven.xml.erb"
         owner "#{node[:jenkins][:server][:user]}"
         group "#{node[:jenkins][:server][:group]}"
+end
+
+template "#{activemq_home}/conf/activemq.xml" do
+        source "activemq.xml.erb"
+        mode 0644
 end
 
