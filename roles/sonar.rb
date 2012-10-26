7f8583b72894f525adf145c6bbf639f449eb45ec
Łukasz Lewczyński <llewczynski@soldevelo.com>
2012-10-26 11:12:59 +0200

    Added sonar role

---
+++ roles/sonar.rb
@@ -0,0 +1,28 @@
+name "sonar"
+description "Sonar server for the MOTECH Platform"
+
+run_list "role[apache-server]",
+        "recipe[mysql::server]",
+        "recipe[sonar]",
+        "recipe[sonar::database_mysql]",
+        "recipe[sonar::proxy_apache]"
+
+override_attributes(
+  :sonar => {
+    :version => "3.3",
+    :checksum => "3e559103f9e2f8505330ad1b33c2382b",
+    :web_domain => "sonar.motechproject.org",
+    :jdbc_url => "jdbc:mysql://localhost:3306/sonar?useUnicode=true&characterEncoding=utf8",
+    :jdbc_driverClassName => "com.mysql.jdbc.Driver",
+    :jdbc_validationQuery => "select 1"
+  },
+  :mysql => {
+    :server_root_password => "password",
+    :bind_address => "localhost"
+  },
+  :authorization => {
+      :sudo => {
+        :include_sudoers_d => true
+      }
+  }
+)
