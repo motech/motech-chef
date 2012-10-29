2e978660940e012086af824a51dd81ee2c544e7f
Paweł Gesek <pgesek@soldevelo.com>
2012-10-29 13:49:53 +0100

    Update db bootstrap in jenkins, mysql added to jenkins role

--- cookbooks/motech-jenkins/recipes/bootstrapdb.rb
+++ cookbooks/motech-jenkins/recipes/bootstrapdb.rb
@@ -7,29 +7,19 @@
 # All rights reserved - Do Not Redistribute
 #
 
-bag = "passwords"
-item = "#{node[:name]}"
+script "permissions" do
+    user "root"
+    interpreter "bash"
+    code <<-EOH
 
-creds = Chef::EncryptedDataBagItem.load(bag, item)
-mysql_pass = creds ? creds["mysql_root_password"] : nil
+    MYSQL_PASS="#{node[:mysql][:server_root_password]}"
+    JENKINS_USER="#{node[:motech_jenkins][:mysql][:user]}"
+    JENKINS_PASS="#{node[:motech_jenkins][:mysql][:password]}"
 
-if mysql_pass
-    script "permissions" do
-        user "root"
-        interpreter "bash"
-        code <<-EOH
+    mysql -u root -p$MYSQL_PASS -e "GRANT ALL ON openmrs.* TO '$JENKINS_USER'@'localhost' IDENTIFIED BY '$JENKINS_PASS' WITH GRANT OPTION"
+    mysql -u root -p$MYSQL_PASS -e "GRANT ALL ON openmrs190.* TO '$JENKINS_USER'@'localhost' IDENTIFIED BY '$JENKINS_PASS' WITH GRANT OPTION"
+    mysql -u root -p$MYSQL_PASS -e "GRANT ALL ON motech_openmrs_api.* TO '$JENKINS_USER'@'localhost' IDENTIFIED BY '$JENKINS_PASS' WITH GRANT OPTION"
+    mysql -u root -p$MYSQL_PASS -e "GRANT ALL ON openmrs_api_test.* TO '$JENKINS_USER'@'localhost' IDENTIFIED BY '$JENKINS_PASS' WITH GRANT OPTION"
 
-        MYSQL_PASS="#{mysql_pass}"
-        JENKINS_USER="#{node[:motech_jenkins][:mysql][:user]}"
-        JENKINS_PASS="#{node[:motech_jenkins][:mysql][:password]}"
-
-        echo $MYSQL_PASS
-
-        mysql -u root -p$MYSQL_PASS -e "GRANT ALL ON openmrs.* TO '$JENKINS_USER'@'localhost' IDENTIFIED BY '$JENKINS_PASS' WITH GRANT OPTION"
-        mysql -u root -p$MYSQL_PASS -e "GRANT ALL ON openmrs190.* TO '$JENKINS_USER'@'localhost' IDENTIFIED BY '$JENKINS_PASS' WITH GRANT OPTION"
-        mysql -u root -p$MYSQL_PASS -e "GRANT ALL ON motech_openmrs_api.* TO '$JENKINS_USER'@'localhost' IDENTIFIED BY '$JENKINS_PASS' WITH GRANT OPTION"
-        mysql -u root -p$MYSQL_PASS -e "GRANT ALL ON openmrs_api_test.* TO '$JENKINS_USER'@'localhost' IDENTIFIED BY '$JENKINS_PASS' WITH GRANT OPTION"
-
-        EOH
-    end
+    EOH
 end
