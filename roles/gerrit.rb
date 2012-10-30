c70378e94abc39753251f405ec382a3b21406d8d
Łukasz Lewczyński <llewczynski@soldevelo.com>
2012-10-30 15:11:50 +0100

    Created gerrit role

---
+++ roles/gerrit.rb
@@ -0,0 +1,14 @@
+name "gerrit"
+description "Gerrit server for the MOTECH Platform"
+
+run_list "role[apache-server]",
+        "recipe[mysql::ruby]",
+        "recipe[mysql::server]",
+        "recipe[gerrit]"
+
+override_attributes(
+  :gerrit => {
+    :hostname => "review.motechproject.org"
+  }
+)
+
